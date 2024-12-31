75e9b735dd1b43072de7271208aab17fb5ed570c
Manfred Spitze <155758012+manfredspitze@users.noreply.github.com>
2024-12-31 15:08:35 -0500

    Update simple_dict_list_comparision.py

--- simple_dict_list_comparision.py
+++ simple_dict_list_comparision.py
@@ -1,28 +1,27 @@
-
+# List of planets
 planets = ['Mars', 'Jupiter', 'Saturn']
 
+# Dictionary containing info about a specific alien
 my_alien = {'first_name' : 'zork', 'home_planet' : 'delta 7'}
 
 
-
-
-
+# Access alien's first name in the dictionary
 print(my_alien['first_name'])
 print()
 
-
+# Build an f-string using key-value pairs from the dictionary
 print(f"{my_alien['first_name'].title()} comes from the planet {my_alien['home_planet'].title()}.")
 print()
 
 
-# Display output
-# print(planets[0])
-# print()
+# Display output from the list
+print(planets[0])
+print()
 
-# print(f'The first planet in the list is: {planets[0]}')
-# print(f'The second planet in the list is: {planets[1]}')
-# print()
+print(f'The first planet in the list is: {planets[0]}')
+print(f'The second planet in the list is: {planets[1]}')
+print()
 
-# last_item_index = len(planets) - 1
-# print(f'The last planet in the list is: {planets[last_item_index]}')
-# print(f'The last planet in the list is: {planets[-1]}')
+last_item_index = len(planets) - 1
+print(f'The last planet in the list is: {planets[last_item_index]}')
+print(f'The last planet in the list is: {planets[-1]}')
