a72f1116ea53d800038bcd2d1a690863d75adda8
Manfred Spitze <155758012+manfredspitze@users.noreply.github.com>
2025-01-23 07:50:45 -0500

    Update cat_update_method_demo.py

--- cat_update_method_demo.py
+++ cat_update_method_demo.py
@@ -19,6 +19,11 @@ print(f"Sepp's age: {cat.get('age', 'Requested key not found in dictionary!') }"
 
 
 # Another example that uses the built-in update ( ) method
+
+# NOTE:
+# If a key in the new dictionary already exists in the original dictionary, the value for that key will be overwritten
+# If a key in the new dictionary does not exist in the original dictionary, a new key-value pair will be added to the dictionary
+
 # Original dictionary
 original_dict = {'apple': 3, 'banana': 2, 'cherry': 5}
 
